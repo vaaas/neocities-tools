f79f40afe844d923d76255258b07a08fb3767dc7
Vas <whiterocket@outlook.com>
2016-05-02 02:14:47 +0300

    brevity

--- nc-sync.py
+++ nc-sync.py
@@ -1,87 +1,68 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import stat
-import json
-import requests
+import sys, os, stat, json, requests
 
-def calc_root():
-	if len(sys.argv) == 2 and sys.argv[1]: return sys.argv[1]
-	else: return "."
-
-def get_auth():
-	user = input("Username: ")
-	password = input("Password: ")
-	return user,password
-
-def calc_remote_files (rootdir, user, password):
+def remote_files (rootdir, user, password):
 	r = requests.get("https://neocities.org/api/list", auth=(user, password))
-	if (r.status_code != 200): sys.exit(1)
+	if (r.status_code != 200): quit()
 	rjson = json.loads(r.text)
-	if (rjson["result"] != "success"): sys.exit(1)
-	remotefiles = dict()
-	for entry in rjson["files"]:
-		pathname = os.path.join(rootdir, entry["path"])
-		remotefiles[pathname] = {"is_directory": entry["is_directory"]}
-		if not entry["is_directory"]:
-			remotefiles[pathname]["size"] = entry["size"]
-	return remotefiles
+	if (rjson["result"] != "success"): quit()
+	return {
+		os.path.join(rootdir, entry["path"]): {
+			"size": entry["size"] if not entry["is_directory"] else None
+		}
+		for entry in rjson["files"]
+	}
 
-def calc_local_files (rootdir):
+def local_files (rootdir):
 	localfiles = dict()
 	for root, dirs, files in os.walk(rootdir):
 		for f in files:
 			if f[0] == ".": continue
 			pathname = os.path.join(root, f)
 			stats = os.stat(pathname)
-			size = stats.st_size
-			is_directory = stat.S_ISDIR(stats.st_mode)
-			localfiles[pathname] = {"is_directory": is_directory}
-			if not is_directory: localfiles[pathname]["size"] = size
+			localfiles[pathname] = { "size": stats.st_size if not stat.S_ISDIR(stats.st_mode) else None }
 	return localfiles
 
 def comparison (remote, local):
-	rset = set(remote)
-	lset = set(local)
+	rset, lset = set(remote), set(local)
 	onlyremote = rset.difference(lset)
 	onlylocal = lset.difference(rset)
 	common = rset.intersection(lset)
 	for f in common:
-		if remote[f]["is_directory"] != local[f]["is_directory"]:
-			print("%s remote and local file types differ.")
-			sys.exit(1)
-		elif remote[f]["is_directory"] is False and remote[f]["size"] != local[f]["size"]:
-			onlylocal.add(f)
+		if remote[f]["size"] != local[f]["size"]:
+			if (remote[f]["size"] == None) or (local[f]["size"] == None):
+				quit("%s remote and local file types differ." % f)
+			else: onlylocal.add(f)
 	return onlyremote, onlylocal
 
 def remove_files (rmlist, rootdir, user, password):
-	if not rmlist: return print("No files to delete")
 	paths = [os.path.relpath(path, rootdir) for path in rmlist]
 	r = requests.post(
 		"https://neocities.org/api/delete",
 		auth=(user, password), 
 		data={ "filenames[]": paths }
 	)
-	print(r.status_code, r.read())
+	print(r.status_code)
 
 def post_files (addlist, rootdir, user, password):
-	if not addlist: return print("No files to upload")
 	files = {os.path.relpath(path, rootdir):open(path, "r") for path in addlist}
 	r = requests.post(
 		"https://neocities.org/api/upload",
 		auth=(user, password),
 		files=files
 	)
-	print(r.status_code, r.read())
+	print(r.status_code)
 
 def main():
-	rootdir = calc_root()
-	user, password = get_auth()
-	remotefiles = calc_remote_files(rootdir, user, password)
-	localfiles = calc_local_files(rootdir)
-	rmlist, addlist = comparison(remotefiles, localfiles)
-	remove_files(rmlist, rootdir, user, password)
-	post_files(addlist, rootdir, user, password)
+	rootdir = sys.argv[1] if len(sys.argv) == 2 and sys.argv[1] else "."
+	user, password = input("Username: "), input("Password: ")
+	rmlist, addlist = comparison(
+		remote_files(rootdir, user, password),
+		local_files(rootdir))
+	if not rmlist: print("No files to delete")
+	else: remove_files(rmlist, rootdir, user, password)
+	if not addlist: print("No files to upload")
+	else: post_files(addlist, rootdir, user, password)
 
-main()
+if __name__ == "__main__": main()
