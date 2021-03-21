64ec7b718b295e5119f4ce65385d6ebaf053c205
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-03-21 15:27:39 +0200

    code golf

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-const L = location
-const D = document
-const qs = q => D.querySelector(q)
-const qss = q => D.querySelectorAll(q)
-const add_class = c => x => x.classList.add(c)
-const remove_class = c => x => x.classList.remove(c)
-
-const filter_clicked = x => filter_posts(x.target.attributes.href.value)
-
-const filter_posts = type =>
-	{ for (x of qss('article'))
-		(type === 'all' || x.attributes.t.value === type ? remove_class('hide') : add_class('hide'))(x)
-	x = qs('a.active') ? remove_class('active')(x) : 0
-	x = qs(`a[href="${type}"]`) ? add_class('active')(x) : 0
-	history.pushState(null, D.title, L.origin+L.pathname+'?'+type) }
-
-if (L.search.length>1)
-	filter_posts(L.search.slice(1))
-for (x of qss('#cats a'))
-	x.onclick = filter_clicked
+L=location
+D=document
+B='attributes'
+C='classList'
+q='querySelector'
+Q='querySelectorAll'
+M=x=>F(x.target[B].href.value)
+F=T=>{for(x of D[Q]('article'))x[C][T=='all'||x[B].t.value==T?'remove':'add']('hide')
+if(x=D[q]('a.active'))x[C].remove('active')
+if(x=D[q](`a[href="${T}"]`))x[C].add('active')
+history.pushState(null,D.title,L.origin+L.pathname+'?'+T)
+return !1}
+for(x of D[Q]('#cats a'))x.onclick=M
+L.search.length>1&&F(L.search.slice(1))
