ed60398a46aca489dfb30f6049e3484612fc49ae
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-03-25 10:25:29 +0200

    even more minification

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ D=document
 B='attributes'
 C='classList'
 q='querySelector'
-Q='querySelectorAll'
+Q=q+'All'
+A='active'
 M=x=>F(x.target[B].href.value)
 F=T=>{for(x of D[Q]('article'))x[C][T=='all'||x[B].t.value==T?'remove':'add']('hide')
-if(x=D[q]('a.active'))x[C].remove('active')
-if(x=D[q](`a[href="${T}"]`))x[C].add('active')
+if(x=D[q]('a.'+A))x[C].remove(A)
+if(x=D[q](`a[href="${T}"]`))x[C].add(A)
 history.pushState(null,D.title,L.origin+L.pathname+'?'+T)
 return !1}
 for(x of D[Q]('#cats a'))x.onclick=M
